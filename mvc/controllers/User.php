48d73446ef0491fa19ab498198286cf4acdbb630
Tran Tuan Anh <anh.tran1819@hcmut.edu.vn>
2022-05-30 11:14:31 +0700

    add header

--- mvc/controllers/User.php
+++ mvc/controllers/User.php
@@ -2,11 +2,6 @@
     header('Access-Control-Allow-Methods: POST');
     header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Methods: DELETE');
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: access");
-    header("Access-Control-Allow-Methods: POST");
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
     require_once "./mvc/models/UserModel.php";
     require_once "./mvc/views/UserView.php";
